7b57e2115fd19ba9ddc10a9a0b1c95a0c4beb08d
CharlesMDF <93060844+CharlesMDF@users.noreply.github.com>
2021-11-14 19:59:33 -0700

    code corrections
    corrected wording on exit prompt to reflect key press

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,9 +1,16 @@
+'''
+Name: Charles Fisher
+Date: 14 November 2021
+Assignment name: Collection Creation
+Purpose: Create a Program to test connectivity with MongoDB
+Sources: Matthes, E. 2019. Python Crash Course 2nd Edition, “A Hands-On, Project-Based Introduction To Programming. No Starch Press, Inc. San Francisco, CA. 
+'''
 from pymongo import MongoClient  
 url = "mongodb+srv://admin:admin@cluster0.qtpgf.mongodb.net/pytech"
 client = MongoClient(url)
 db = client.pytech 
+
 print("-- Pytech Collelection List --")
 print(db.list_collection_names())
-
-print("End of program, press any key to exit... ")
+print("End of program, press Enter to exit... ")
 input() #flag to exit 
